acca4068795c30172bf6bc1a28448a108e84d2b9
CarolTeodoro <caroline.r.teodoro@gmail.com>
2022-06-09 16:36:30 -0300

    primeiro commit

---
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -0,0 +1,38 @@
+package me.dio.academia.digital.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+//As anotações abaixo são do lombok
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+//As anotações abaixo são referentes a persistência do banco de dados
+@Entity
+@Table(name = "tb_avaliacoes")
+public class AvaliacaoFisica {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+  private Long id;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "aluno_id")
+  private Aluno aluno;
+
+  private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
+
+  @Column(name = "peso_atual")
+  private double peso;
+
+  @Column(name = "altura_atual")
+  private double altura;
+
+}
